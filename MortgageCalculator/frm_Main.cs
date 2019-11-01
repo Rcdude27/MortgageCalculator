6323ee185ca1b065d93c30c7e64d8829cc9bd29e
Bryce Bontz <bontzbryce@gmail.com>
2019-10-31 20:48:12 -0500

    .DLL project, reference, and code implemented. Functioning remains the same.

--- MortgageCalculator/frm_Main.cs
+++ MortgageCalculator/frm_Main.cs
@@ -7,28 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MortgagePaymentCalculation;
 
 namespace MortgageCalculator {
+
     public partial class frm_Main : Form {
-        public frm_Main() {
+        public frm_Main() 
+        {
             InitializeComponent();
         }
 
+        MortgageCalculations mcCal = new MortgageCalculations();
+
         private void btnCalc_Click(object sender, EventArgs e) 
         {
-           txtPayment.Text = MortgageCalc(Convert.ToDouble(txtPrinc.Text), Convert.ToDouble(txtInterest.Text), Convert.ToDouble(txtTerm.Text));
-        }
-
-        private string MortgageCalc(double dPrinciple, double dAIR, double dTerm) 
-        {
-            double dTermInMonths = dTerm * 12;
-            double dMonthlyInterestRate = (dAIR / 12) / 100;
-            double d1PlusR = Math.Pow(1 + dMonthlyInterestRate, dTermInMonths);
-            double dNumerator = dMonthlyInterestRate * d1PlusR;
-            double dDenominator = d1PlusR - 1;
-            double dMortgagePayment = dPrinciple * (dNumerator / dDenominator);
-
-            return dMortgagePayment.ToString("C2");
+            txtPayment.Text = mcCal.MortgageCalc(Convert.ToDouble(txtPrinc.Text), Convert.ToDouble(txtInterest.Text), Convert.ToDouble(txtTerm.Text));
         }
     }
 }
---
+++ MortgagePaymentCalculation/MortgageCalculations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MortgagePaymentCalculation
+{
+    public class MortgageCalculations
+    {      
+        public string MortgageCalc(double dPrinciple, double dAIR, double dTerm) 
+        {
+            double dTermInMonths = dTerm * 12;
+            double dMonthlyInterestRate = (dAIR / 12) / 100;
+            double d1PlusR = Math.Pow(1 + dMonthlyInterestRate, dTermInMonths);
+            double dNumerator = dMonthlyInterestRate * d1PlusR;
+            double dDenominator = d1PlusR - 1;
+            double dMortgagePayment = dPrinciple * (dNumerator / dDenominator);
+
+            return dMortgagePayment.ToString("C2");
+        }
+    }
+}
