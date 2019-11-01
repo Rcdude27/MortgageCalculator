497c84bd5111fb2e7ec2716b8b7e769a6474797b
Dimitri Vanzin <bontzbryce@gmail.com>
2019-11-01 10:43:53 -0500

    FINISHED! GRADE THIS ONE! Fixed bug where decimal point could not be placed in interest textbox.

--- MortgageCalculator/frm_Main.cs
+++ MortgageCalculator/frm_Main.cs
@@ -26,7 +26,7 @@ namespace MortgageCalculator {
 
         private void isTextEmpty(string TextBoxText)
         {
-            if (TextBoxText == "")
+            if (TextBoxText == "" || TextBoxText == ".")
                 btnCalc.Enabled = false;
             else
                 btnCalc.Enabled = true;
@@ -40,7 +40,11 @@ namespace MortgageCalculator {
         private void txtInterest_KeyPress(object sender, KeyPressEventArgs e)
         {
             isTextEmpty(txtInterest.Text);
-            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+                e.Handled = true;
+            // only allow one decimal point
+            if (e.KeyChar == '.' && (sender as TextBox).Text.IndexOf('.') > -1)
+                e.Handled = true;
         }
         private void txtTerm_KeyPress(object sender, KeyPressEventArgs e)
         {
